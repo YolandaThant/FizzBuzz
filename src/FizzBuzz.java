13c01a217ed8d087558d812ec39452f6cfc0fcae
YolandaThant <yolanda.lee159@gmail.com>
2024-09-07 18:50:00 -0400

    added doFizzBuzz

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -6,18 +6,7 @@ class FizzBuzz {
     public static void main(String[] args) {
         int i = 1;
         while (i < 100) {
-            boolean divisibleBy3 = i % 3 == 0;
-            boolean divisibleBy5 = i % 5 == 0;
-            if (divisibleBy3 && divisibleBy5) {
-                System.out.println("FizzBuzz");
-            } else if (divisibleBy3) {
-                System.out.println("Fizz");
-            } else if (divisibleBy5) {
-                System.out.println("Buzz");
-            } else {
-                System.out.println(i);
-            }
-            i++;
+            i = doFizzBuzz(i);
         }
 
 //        for (int i = 1; i < 100; i++) {
@@ -45,5 +34,21 @@ class FizzBuzz {
 //
 //            }
 //        }
+    }
+
+    private static int doFizzBuzz(int i) {
+        boolean divisibleBy3 = i % 3 == 0;
+        boolean divisibleBy5 = i % 5 == 0;
+        if (divisibleBy3 && divisibleBy5) {
+            System.out.println("FizzBuzz");
+        } else if (divisibleBy3) {
+            System.out.println("Fizz");
+        } else if (divisibleBy5) {
+            System.out.println("Buzz");
+        } else {
+            System.out.println(i);
+        }
+        i++;
+        return i;
     }
 }
